88d1751f25f556b9691da7114b9b0b31c1c640c4
Patrik Valo <xvalo2@fi.muni.cz>
2019-10-27 20:51:06 +0100

    progress bar

--- client/src/components/ProgressBar.vue
+++ client/src/components/ProgressBar.vue
@@ -22,22 +22,24 @@ import Component from "vue-class-component";
     }
 })
 export default class ProgressBar extends Vue {
+    private _interval: number | undefined; // setInterval
     public percentage: number = 0;
-    private _interval: number = 0;
 
     public constructor() {
         super();
     }
 
     public mounted() {
+        // rendering each 500ms
         this._interval = setInterval(() => {
-            if (this.percentage == 100) {
-                clearInterval(this._interval);
-            }
             const uploaded = this.$props.uploaded;
             const total = this.$props.total;
             this.percentage = Math.round((uploaded / total) * 100);
         }, 500);
+    }
+
+    public destroyed() {
+        clearInterval(this._interval);
     }
 }
 </script>
@@ -45,6 +47,7 @@ export default class ProgressBar extends Vue {
 .progress-bar-area {
     padding: 15px 0 15px 0;
 }
+
 .percentage {
     text-align: center;
 }
