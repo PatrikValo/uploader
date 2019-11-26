a5081e8d7aaa6d76ca0a8b4fbdb7fb5b71132616
Patrik Valo <xvalo2@fi.muni.cz>
2019-11-26 15:31:47 +0100

    config

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.OPENSHIFT_NODEJS_PORT || 9998,
+    port: process.env.PORT || 9998,
     ivSize: 16,
     chunkSize: 64 * 1024,
     spacePath: __dirname + "/files"
