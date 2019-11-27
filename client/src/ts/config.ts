c4785fb26f700f58f0504fb953cc1a4c24b2614e
Patrik Valo <xvalo2@fi.muni.cz>
2019-11-27 11:26:32 +0100

    config

--- client/src/ts/config.ts
+++ client/src/ts/config.ts
@@ -1,13 +1,13 @@
 class Server {
-    public readonly host: string = "aploader.herokuapp.com";
-    public readonly port: string = "443";
-    public readonly protocol: string = "https";
+    public readonly host: string = "localhost";
+    public readonly port: string = "9998";
+    public readonly protocol: string = "http";
 }
 
 class Client {
-    public readonly host: string = "aploader.herokuapp.com";
-    public readonly port: string = "443";
-    public readonly protocol: string = "https";
+    public readonly host: string = "localhost";
+    public readonly port: string = "8080";
+    public readonly protocol: string = "http";
     public readonly fileSizeLimit: number = 1024 * 1024 * 1024;
     public readonly chunkSize: number = 64 * 1024;
 }
