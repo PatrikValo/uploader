3236cb45c1109c592398e0252a2e08353f303c62
Patrik Valo <xvalo2@fi.muni.cz>
2019-11-27 22:21:26 +0100

    utils port tests

--- tests/client/utils.test.ts
+++ tests/client/utils.test.ts
@@ -4,43 +4,43 @@ import Utils from "../../client/src/ts/utils";
 describe("Utils tests", () => {
     describe("Server part", () => {
         const pro = Config.server.protocol;
-        const port = Config.server.port;
+        const port = Config.server.port ? ":" + Config.server.port : "";
         const host = Config.server.host;
         describe("WebsocketUrl", () => {
             const p = pro === "http" ? "ws" : "wss";
             test("It should return correct url without path as parameter", () => {
-                const correct = `${p}://${host}:${port}`;
+                const correct = `${p}://${host}${port}`;
                 const result = Utils.server.websocketUrl();
                 expect(result).toBe(correct);
             });
             test("It should return correct url with slash before path as parameter", () => {
                 const path = "/download/websocket/abc";
-                const correct = `${p}://${host}:${port}${path}`;
+                const correct = `${p}://${host}${port}${path}`;
                 const result = Utils.server.websocketUrl(path);
                 expect(result).toBe(correct);
             });
             test("It should return correct url without slash before path as parameter", () => {
                 const path = "download/websocket/abc";
-                const correct = `${p}://${host}:${port}/${path}`;
+                const correct = `${p}://${host}${port}/${path}`;
                 const result = Utils.server.websocketUrl(path);
                 expect(result).toBe(correct);
             });
         });
         describe("ClassicUrl", () => {
             test("It should return correct url without path as parameter", () => {
-                const correct = `${pro}://${host}:${port}`;
+                const correct = `${pro}://${host}${port}`;
                 const result = Utils.server.classicUrl();
                 expect(result).toBe(correct);
             });
             test("It should return correct url with slash before path as parameter", () => {
                 const path = "/download/abc";
-                const correct = `${pro}://${host}:${port}${path}`;
+                const correct = `${pro}://${host}${port}${path}`;
                 const result = Utils.server.classicUrl(path);
                 expect(result).toBe(correct);
             });
             test("It should return correct url without slash before path as parameter", () => {
                 const path = "download/abc";
-                const correct = `${pro}://${host}:${port}/${path}`;
+                const correct = `${pro}://${host}${port}/${path}`;
                 const result = Utils.server.classicUrl(path);
                 expect(result).toBe(correct);
             });
@@ -50,52 +50,52 @@ describe("Utils tests", () => {
     describe("Client part", () => {
         describe("BuildUrl", () => {
             const pro = Config.client.protocol;
-            const port = Config.client.port;
+            const port = Config.client.port ? ":" + Config.client.port : "";
             const host = Config.client.host;
             test("It should return correct url with empty each parameter", () => {
-                const correct = `${pro}://${host}:${port}/`;
+                const correct = `${pro}://${host}${port}/`;
                 const result = Utils.buildUrl("", "", "");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url with empty base parameter", () => {
-                const correct = `${pro}://${host}:${port}/25897-56#258_25`;
+                const correct = `${pro}://${host}${port}/25897-56#258_25`;
                 const result = Utils.buildUrl("", "25897-56", "258_25");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url with empty base parameter and key parameter", () => {
-                const correct = `${pro}://${host}:${port}/25897-56`;
+                const correct = `${pro}://${host}${port}/25897-56`;
                 const result = Utils.buildUrl("", "25897-56", "");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url with empty id parameter", () => {
-                const correct = `${pro}://${host}:${port}/download#25`;
+                const correct = `${pro}://${host}${port}/download#25`;
                 const result = Utils.buildUrl("download", "", "25");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url with empty id parameter and key parameter", () => {
-                const correct = `${pro}://${host}:${port}/download`;
+                const correct = `${pro}://${host}${port}/download`;
                 const result = Utils.buildUrl("download", "", "");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url with empty key parameter", () => {
-                const correct = `${pro}://${host}:${port}/download/25`;
+                const correct = `${pro}://${host}${port}/download/25`;
                 const result = Utils.buildUrl("download", "25", "");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url with hash character before key parameter", () => {
-                const correct = `${pro}://${host}:${port}/download/25#125`;
+                const correct = `${pro}://${host}${port}/download/25#125`;
                 const result = Utils.buildUrl("download", "25", "#125");
                 expect(result).toBe(correct);
             });
 
             test("It should return correct url each parameter is given", () => {
-                const correct = `${pro}://${host}:${port}/download/25#125`;
+                const correct = `${pro}://${host}${port}/download/25#125`;
                 const result = Utils.buildUrl("download", "25", "125");
                 expect(result).toBe(correct);
             });
