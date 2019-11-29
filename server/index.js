4c59544790cfb185fd94dcc81510661d85cea3eb
Patrik Valo <xvalo2@fi.muni.cz>
2019-11-30 00:33:44 +0100

    server change

--- server/index.js
+++ server/index.js
@@ -10,14 +10,6 @@ const path = require("path");
 const app = express();
 expressWs(app);
 
-if (config.environment === "production") {
-    app.use(function(req, res) {
-        if (!req.secure) {
-            res.redirect("https://" + req.headers.host + req.url);
-        }
-    });
-}
-
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
