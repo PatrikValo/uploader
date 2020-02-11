a1a41b107e88eb696af169c4288d3ab3c457ffe8
Patrik Valo <xvalo2@fi.muni.cz>
2020-02-11 22:25:01 +0100

    auth route

---
+++ client/src/components/routes/Auth.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-container class="h-100">
+        <loading-page></loading-page>
+    </b-container>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import Vue from "vue";
+import LoadingPage from "../LoadingPage.vue";
+import AuthDropbox from "../../ts/authDropbox";
+
+@Component({
+    components: { LoadingPage }
+})
+export default class Auth extends Vue {
+    public constructor() {
+        super();
+    }
+
+    public mounted() {
+        const hash = this.$route.hash;
+        if (!hash) {
+            this.$router.push("/");
+            return (location.href = "/");
+        }
+
+        try {
+            const auth = new AuthDropbox();
+            auth.logIn(hash);
+            location.href = "/";
+        } catch (e) {
+            location.href = "/";
+        }
+    }
+}
+</script>
