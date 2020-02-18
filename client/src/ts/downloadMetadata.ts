0fb37ed32aa6aa08747d44915e8a4e1098aad92b
Patrik Valo <xvalo2@fi.muni.cz>
2020-02-18 15:02:37 +0100

    downloadMetadata doc

--- client/src/ts/downloadMetadata.ts
+++ client/src/ts/downloadMetadata.ts
@@ -32,24 +32,53 @@ abstract class DownloadMetadata implements IDownloadMetadata {
         };
     }
 
+    /**
+     * It downloads the part of file, whose first position in file is defined by
+     * start param and last position is defined by (end - 1) param.
+     *
+     * [start, end) interval
+     * @param start
+     * @param end
+     * @return Promise with data
+     */
     protected abstract range(start: number, end: number): Promise<Uint8Array>;
 
+    /**
+     * It downloads initialisation vector for encrypting
+     *
+     * @return Promise with iv
+     */
     private getIv(): Promise<Uint8Array> {
         return this.range(0, Config.cipher.saltLength);
     }
 
+    /**
+     * It downloads flag, which stores information about existence of password
+     *
+     * @return Promise with flag
+     */
     private getFlag(): Promise<Uint8Array> {
         const start = Config.cipher.saltLength;
         const end = start + 1;
         return this.range(start, end);
     }
 
+    /**
+     * It downloads salt, which is used for deriving key from password
+     *
+     * @return Promise with salt
+     */
     private getSalt(): Promise<Uint8Array> {
         const start = Config.cipher.ivLength + 1;
         const end = start + Config.cipher.saltLength;
         return this.range(start, end);
     }
 
+    /**
+     * It downloads length of metadata
+     *
+     * @return Promise with length of metadata
+     */
     private async getLength(): Promise<number> {
         const start = Config.cipher.ivLength + 1 + Config.cipher.saltLength;
         const end = start + 2;
@@ -57,6 +86,12 @@ abstract class DownloadMetadata implements IDownloadMetadata {
         return Metadata.lengthToNumber(uint);
     }
 
+    /**
+     * It downloads metadata of file
+     *
+     * @param length - length of metadata
+     * @return Promise with metadata
+     */
     private getMetadata(length: number): Promise<Uint8Array> {
         const start = Config.cipher.ivLength + 1 + Config.cipher.saltLength + 2;
         const end = start + length;
