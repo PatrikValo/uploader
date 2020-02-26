8a7ebf23fb6a6a79f25f25b243b094910b499d2b
Patrik Valo <xvalo2@fi.muni.cz>
2020-02-26 16:09:09 +0100

    gib to gb

--- client/src/ts/config.ts
+++ client/src/ts/config.ts
@@ -18,8 +18,8 @@ class Client {
     public readonly host: string = environment.HOST || "localhost";
     public readonly port: string = production ? "" : "8080";
     public readonly protocol: string = production ? "https" : "http";
-    public readonly fileSizeLimit: number = 1024 * 1024 * 1024;
-    public readonly fileSizeLimitDropbox: number = 1024 * 1024 * 1024 * 350;
+    public readonly fileSizeLimit: number = 1000 * 1000 * 1000;
+    public readonly fileSizeLimitDropbox: number = 1000 * 1000 * 1000 * 350;
     public readonly chunkSize: number = 64 * 1024;
     public readonly blobFileSizeLimit: number = 1024 * 1024 * 250;
 }
